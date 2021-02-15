42c7304a4c765f12c4f89202fda3e4ca8c2764db
Maxohayce <Maxohayce.devmax.corl@gmail.com>
2021-02-15 15:39:18 +0100

    feat: Pre-deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   render = () => {
     return (
-      <Router>
+      <Router basename="/">
         {/* <Toolbar /> */}
         {/* <SideDrawer /> */}
 
--- src/components/containers/CreateYours/CreateYours.js
+++ src/components/containers/CreateYours/CreateYours.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Layout  from '../../presentational/Layout/index';
+import './About.css';
+import Lottie from 'react-lottie'
+import animationData from '../../../assets/LottieFiles/construction.json';
 
-const CreateYours = () => (
-    <Layout>
-        Create yours page
-    </Layout>
-);
+const CreateYours = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+      return(
+     <Layout>
+         <div className="body">
+            <Lottie 
+                options={defaultOptions}
+                height={400}
+                width={400}
+            />
+            <h3 className="oops">Oops! Page is still under construction</h3>
+            <h4 className="sorry">Sorry for the inconveniences</h4>
+          </div>
+     </Layout>
+    
+    );
+  };
 
 export default CreateYours;
